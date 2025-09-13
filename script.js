a6d1df736a6ca549a8dc77d2e192f0621754bcb1
apocode01 <apopanou@gmail.com>
2025-09-13 22:40:35 +0300

    styling with overlay support

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const navMenuOpenBtn = document.querySelector(`.nav-menu-open`);
 const navMenuCloseBtn = document.querySelector(`.nav-menu-close`);
 const menu = document.querySelector(`nav`);
+const closedOverlay = document.querySelector(`.closed-overlay`);
 
 navMenuOpenBtn.addEventListener('click', () => {
     menu.classList.add(`open`);
+    closedOverlay.classList.add(`enabled`);
 })
 
 navMenuCloseBtn.addEventListener('click', () => {
     menu.classList.remove(`open`);
+    closedOverlay.classList.remove(`enabled`);
 })
