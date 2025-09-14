f608a07c534c20462a8e66a9dcc0fd89a443c146
apocode01 <apopanou@gmail.com>
2025-09-14 19:54:44 +0300

    better menu transitioning and functionality

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const navMenuOpenBtn = document.querySelector(`.nav-menu-open`);
 const navMenuCloseBtn = document.querySelector(`.nav-menu-close`);
 const menu = document.querySelector(`nav`);
-const closedOverlay = document.querySelector(`.closed-overlay`);
+const closedOverlay = document.querySelector(`.overlay`);
 
 navMenuOpenBtn.addEventListener('click', () => {
-    menu.classList.add(`open`);
+    if (menu.classList.contains(`closed`)) {
+        menu.classList.replace(`closed`, `open`);
+    }
+    else menu.classList.add(`open`);
     closedOverlay.classList.add(`enabled`);
 })
 
 navMenuCloseBtn.addEventListener('click', () => {
-    menu.classList.remove(`open`);
+    menu.classList.replace(`open`, `closed`);
     closedOverlay.classList.remove(`enabled`);
 })
+
+const mq = window.matchMedia("(min-width: 29.5rem)");
+mq.addEventListener("change", (e) => {
+    if (e.matches) {
+        menu.classList.remove("closed", "open");
+        closedOverlay.classList.remove(`enabled`);
+    }
+});
